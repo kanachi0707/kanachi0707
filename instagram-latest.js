50bd9f8f3860622202472910ac6884344f7e470a
kanachi0707 <kanachi607@gmail.com>
2026-07-16 12:00:06 +0900

    Update Instagram latest

--- instagram-latest.js
+++ instagram-latest.js
@@ -1,9 +1,9 @@
 ﻿window.__INSTAGRAM_LATEST_POST__ = {
-    "url":  "https://www.instagram.com/p/Day4L2UB1U1/",
-    "caption":  "#GoodMorningKanachi☀️\n\n他人の幸せは手に入れることができない\n自分の幸せに集中しよう\n\nYou can’t live someone else’s happiness.\nFocus on your own.",
-    "updatedAt":  "2026-07-15T12:00:08+09:00",
+    "url":  "https://www.instagram.com/p/Da1cy9-Bmd1/",
+    "caption":  "留まる　流れる\n繰り返しながら旅をするように生きる\nだから上手くいく\n\nPause. Flow.\nJourney through both.\nThat’s how it works.\n\n#GoodMorningKanachi",
+    "updatedAt":  "2026-07-16T12:00:06+09:00",
     "fallbackUrl":  "https://www.instagram.com/p/DVM3FVxErTp/",
     "source":  "instagram-web-profile-api",
     "status":  "ok",
-    "postTakenAt":  "2026-07-15T01:24:13+00:00"
+    "postTakenAt":  "2026-07-16T01:22:36+00:00"
 };
