f2e533fa8ccbe455b9656bff4d3fdc1003426311
kanachi0707 <kanachi607@gmail.com>
2026-07-21 12:00:06 +0900

    Update Instagram latest

--- instagram-latest.js
+++ instagram-latest.js
@@ -1,9 +1,9 @@
 ﻿window.__INSTAGRAM_LATEST_POST__ = {
-    "url":  "https://www.instagram.com/p/Da_XwAESEWu/",
-    "caption":  "#GoodMorningKanachi\n\n大人だって真剣に遊ぼう\n遊びは最高の学び\n\nEven adults should play with all their heart. \nPlay is one of the best ways to learn.",
-    "updatedAt":  "2026-07-20T16:12:50+09:00",
+    "url":  "https://www.instagram.com/p/DbCbfj3halN/",
+    "caption":  "#GoodMorningKanachi \n\n自分は強いと主張するのは弱さを隠すため\n本当の強さは他者の言葉で語られる\n\nThose who claim to be strong \noften hide their weakness. \nTrue strength is recognized by others.",
+    "updatedAt":  "2026-07-21T12:00:05+09:00",
     "fallbackUrl":  "https://www.instagram.com/p/DVM3FVxErTp/",
     "source":  "instagram-web-profile-api",
     "status":  "ok",
-    "postTakenAt":  "2026-07-19T21:51:06+00:00"
+    "postTakenAt":  "2026-07-21T02:21:15+00:00"
 };
