6d9e67706a69dddafcb29bc3ace27c8dde415080
kanachi0707 <kanachi607@gmail.com>
2026-07-22 12:00:04 +0900

    Update Instagram latest

--- instagram-latest.js
+++ instagram-latest.js
@@ -1,9 +1,9 @@
 ﻿window.__INSTAGRAM_LATEST_POST__ = {
-    "url":  "https://www.instagram.com/p/DbCbfj3halN/",
-    "caption":  "#GoodMorningKanachi \n\n自分は強いと主張するのは弱さを隠すため\n本当の強さは他者の言葉で語られる\n\nThose who claim to be strong \noften hide their weakness. \nTrue strength is recognized by others.",
-    "updatedAt":  "2026-07-21T12:00:05+09:00",
+    "url":  "https://www.instagram.com/p/DbEwbNLh32E/",
+    "caption":  "#GoodMorningKanachi \n\n少数派でいい\n多数派と思われた瞬間、私が平凡なものに変わってしまう気がして\n\nI’m content being in the minority. \nThe crowd isn’t where I find myself.",
+    "updatedAt":  "2026-07-22T12:00:03+09:00",
     "fallbackUrl":  "https://www.instagram.com/p/DVM3FVxErTp/",
     "source":  "instagram-web-profile-api",
     "status":  "ok",
-    "postTakenAt":  "2026-07-21T02:21:15+00:00"
+    "postTakenAt":  "2026-07-22T00:02:43+00:00"
 };
