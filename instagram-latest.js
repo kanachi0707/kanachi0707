c54fc5230b5bea4ce7f9c524e7cc08059291b907
kanachi0707 <kanachi607@gmail.com>
2026-07-24 12:00:04 +0900

    Update Instagram latest

--- instagram-latest.js
+++ instagram-latest.js
@@ -1,9 +1,9 @@
 ﻿window.__INSTAGRAM_LATEST_POST__ = {
-    "url":  "https://www.instagram.com/p/DbHSiR2h2mf/",
-    "caption":  "#GoodMorningKanachi \n\n自分が幸せだって思えた時が、\n誰かを応援できる時\n\nI’m happy,\nso I can support others.",
-    "updatedAt":  "2026-07-23T12:00:04+09:00",
+    "url":  "https://www.instagram.com/p/DbKJyC5BkW0/",
+    "caption":  "#GoodMorningKanachi \n\n子どもの頃の自分に、 \n胸を張って会える大人でいたい\n\nI want to be someone my younger self would be proud of.",
+    "updatedAt":  "2026-07-24T12:00:04+09:00",
     "fallbackUrl":  "https://www.instagram.com/p/DVM3FVxErTp/",
     "source":  "instagram-web-profile-api",
     "status":  "ok",
-    "postTakenAt":  "2026-07-22T23:39:13+00:00"
+    "postTakenAt":  "2026-07-24T02:20:25+00:00"
 };
