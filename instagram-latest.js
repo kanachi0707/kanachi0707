1365d8e39fa3df4583e8e37d3e844e469e6981e3
kanachi0707 <kanachi607@gmail.com>
2026-08-10 12:00:05 +0900

    Update Instagram latest

--- instagram-latest.js
+++ instagram-latest.js
@@ -1,9 +1,9 @@
 ﻿window.__INSTAGRAM_LATEST_POST__ = {
-    "url":  "https://www.instagram.com/p/DbxAghRz63H/",
-    "caption":  "🎵New Music Video\n\nkanachi - \"まってる\" Music Video\nhttps://www.youtube.com/watch?v=XJ2z0IblVCQ\n（kanachi） \n\n#suno #kanachi",
-    "updatedAt":  "2026-08-09T12:00:05+09:00",
+    "url":  "https://www.instagram.com/p/Db10u3JB0Sd/",
+    "caption":  "#GoodMorningKanachi \n\n空を飛びたいと願った \n遠くの人と話したいと願った \n人類の強い意思は、不可能を可能にしてきた \n私たちの意思は、未来を変えられる\n\nWe dreamed of flying through the sky. \nWe dreamed of talking to people far away. \nHumanity’s strong will has made the impossible possible. \nOur will can change the future",
+    "updatedAt":  "2026-08-10T12:00:04+09:00",
     "fallbackUrl":  "https://www.instagram.com/p/DVM3FVxErTp/",
     "source":  "instagram-web-profile-api",
     "status":  "ok",
-    "postTakenAt":  "2026-08-08T04:30:16+00:00"
+    "postTakenAt":  "2026-08-10T01:23:06+00:00"
 };
