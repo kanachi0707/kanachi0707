b5e08f1114408f707c1675f462994554b2556bfb
kanachi0707 <kanachi607@gmail.com>
2026-08-15 13:09:34 +0900

    Update Instagram latest

--- instagram-latest.js
+++ instagram-latest.js
@@ -1,9 +1,9 @@
 ﻿window.__INSTAGRAM_LATEST_POST__ = {
-    "url":  "https://www.instagram.com/p/Db9tBF9z9Ke/",
-    "caption":  "Making MV \"まってる\"\n\nMVフルはハイライト→MusicVideoにリンクがあるよ！観てね～✨",
-    "updatedAt":  "2026-08-13T12:00:06+09:00",
+    "url":  "https://www.instagram.com/p/DcClCEdhQOZ/",
+    "caption":  "#GoodMorningKanachi \n\n誰かに認められるためじゃない \n自分を信じるために努力を積み重ねよう\n\nKeep putting in the effort. \nIt will give you the strength to believe in yourself.",
+    "updatedAt":  "2026-08-15T13:09:33+09:00",
     "fallbackUrl":  "https://www.instagram.com/p/DVM3FVxErTp/",
     "source":  "instagram-web-profile-api",
     "status":  "ok",
-    "postTakenAt":  "2026-08-13T02:50:07+00:00"
+    "postTakenAt":  "2026-08-15T00:16:01+00:00"
 };
