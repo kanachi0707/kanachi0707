ca19d9578532e0436a2f9d4bdbdb3420589b0833
kanachi0707 <kanachi607@gmail.com>
2026-08-22 12:45:32 +0900

    Update Instagram latest

--- instagram-latest.js
+++ instagram-latest.js
@@ -1,9 +1,9 @@
 ﻿window.__INSTAGRAM_LATEST_POST__ = {
-    "url":  "https://www.instagram.com/p/DcPd726hE-c/",
-    "caption":  "#GoodMorningKanachi\n\n学ぶことは、答えを持つこと。\n学び続けることは、その答えを疑うこと。\n\nTo learn is to find answers.\nTo keep learning is to keep questioning them.",
-    "updatedAt":  "2026-08-20T12:00:06+09:00",
+    "url":  "https://www.instagram.com/p/DcU4ipLBPdr/",
+    "caption":  "#GoodMorningKanachi \n\n誰かに食ってかかることでしか自分を示せないなら\nその言葉が映しているのは相手ではなく、自分の未熟さだ。\n\nIf the only way you can assert yourself is by attacking others, \nyour words reveal not who they are, but how much growing you still have to do.",
+    "updatedAt":  "2026-08-22T12:45:31+09:00",
     "fallbackUrl":  "https://www.instagram.com/p/DVM3FVxErTp/",
     "source":  "instagram-web-profile-api",
     "status":  "ok",
-    "postTakenAt":  "2026-08-20T00:24:03+00:00"
+    "postTakenAt":  "2026-08-22T02:52:44+00:00"
 };
